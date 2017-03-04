2d0a2d968347fdbc2864ac3faabe20b1e66daf13
doxramos <green.morgan87@gmail.com>
2017-03-03 22:37:27 -0800

    Added footer to theme, Began work on install directory

--- public_html/index.php
+++ public_html/index.php
@@ -5,17 +5,29 @@
  * Date: 1/19/2017
  * Time: 9:03 AM
  */
-require_once("../config.php");
-if(devBuild == true && publicBuild == false && SystemMessages == true) {
-    echo "<b>This is a developer release. Please only use this release if you know what you are doing!<br />Version ".VersionNO." ".VersionName.' Release<br />Released '.ReleaseDate.'</b>';
-}
-LoadPlugins();
-if(!isset($_GET['page'])) {
-    LoadTheme(ThemePath);
-} else {
-    switch($_GET['page']) {
-        case "admin": echo "Load Admin Page"; break;
-        default: LoadTheme(ThemePath); break;
+if(!file_exists("../config.json")) {
+    if(file_exists("../install/index.php")) {
+        require_once "../install/index.php";
+    } else {
+        die("Installation Files Corrupted");
     }
+} else {
+    require_once("../config.php");
+    if (devBuild == true && publicBuild == false && SystemMessages == true) {
+        echo "<b>This is a developer release. Please only use this release if you know what you are doing!<br />Version " . VersionNO . " " . VersionName . ' Release<br />Released ' . ReleaseDate . '</b>';
+    }
+    LoadPlugins();
+    if (!isset($_GET['page'])) {
+        LoadTheme(ThemePath);
+    } else {
+        switch ($_GET['page']) {
+            case "admin":
+                echo "Load Admin Page";
+                break;
+            default:
+                LoadTheme(ThemePath);
+                break;
+        }
+    }
+    LoadPluginScripts();
 }
-LoadPluginScripts();
